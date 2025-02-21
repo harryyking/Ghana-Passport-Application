bf9993e8e5fe1fb44780d7809abbafad50c83e09
harryyking <arthurharry06@gmail.com>
2025-02-21 01:39:51 +0000

    removed google api key

--- lib/googleMapsConfig.ts
+++
@@ -1,8 +0,0 @@
-// import type { LoadScriptProps } from "@react-google-maps/api"
-
-// export const googleMapsConfig: LoadScriptProps = {
-//   id: "google-map-script",
-//   googleMapsApiKey: "AIzaSyAuU4DO4sxHLljpOJnPCWXgIOMpGIn3BN0",
-//   libraries: ["places", "maps"],
-// }
-
